df2a7e27fd1753f2104189c90e2aab96bb314dfc
Ryan <linr3@sunypoly.edu>
2023-09-04 10:18:53 -0400

    gladiator

--- assign01/src/main/java/edu/linr3/assign01/Greetings.java
+++ assign01/src/main/java/edu/linr3/assign01/Greetings.java
@@ -4,6 +4,6 @@ public class Greetings
 {
     public static void main(String[]args)
     {
-        System.out.println("Hello World");
+        System.out.println("Are you not entertained?!");
     }
 }
